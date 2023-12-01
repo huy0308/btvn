9d29813e1d03aead45e69e6cb92ee23201d273d4
unknown <lyhuongltt@gmail.com>
2023-12-01 19:07:15 +0700

    btvnb2

---
+++ bt2.js
@@ -0,0 +1,27 @@
+let basket = ["grapes",15]
+console.log("i have " + basket[1] + " " + basket[0])
+
+//cach 1
+let x = 1
+while (x<=15){
+    console.log(basket[0])
+    x++
+}
+
+//cach 2
+let y = 1
+do{
+    console.log(basket[0])
+    y++
+}while(y<=15)
+
+//cach 3
+for (z = 1; z<=15; z++){
+    console.log(basket[0])
+}
+
+var myDog = {
+    "name": "Husky",
+    "legs": 4,
+    "color": Brown
+}
